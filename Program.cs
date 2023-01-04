827dfbae217dd1a9bb7ba51fed6591d9f6e61c05
barpaw <barpaw@gmail.com>
2023-01-04 14:13:27 +0100

    fix: message format

--- Program.cs
+++ Program.cs
@@ -30,9 +30,12 @@ app.MapGet("/health", () =>
     return Results.Ok();
 });
 
-app.MapPost("/handle-fail2ban-webhook", async (Fail2BanWebhook fail2BanWebhook, IHttpClientFactory httpClientFactory) =>
+app.MapPost("/handle-fail2ban-webhook", async (Fail2BanWebhook fail2BanWebhook, IHttpClientFactory httpClientFactory, ILoggerFactory loggerFactory) =>
 {
-
+    var logger = loggerFactory.CreateLogger("");
+    
+    logger.LogInformation("{Message}", fail2BanWebhook.Message);
+    
     var client = httpClientFactory.CreateClient();
     
     string matrix_notifier_url = Environment.GetEnvironmentVariable("MATRIX_NOTIFIER_URL");
@@ -42,15 +45,19 @@ app.MapPost("/handle-fail2ban-webhook", async (Fail2BanWebhook fail2BanWebhook, 
     string matrix_homeserver_user = Environment.GetEnvironmentVariable("MATRIX_HOMESERVER_USER");
     string matrix_homeserver_passwd = Environment.GetEnvironmentVariable("MATRIX_HOMESERVER_PASSWD");
     string matrix_homeserver_room = Environment.GetEnvironmentVariable("MATRIX_HOMESERVER_ROOM");
+    
+    string matrix_message_footer = Environment.GetEnvironmentVariable("MATRIX_MESSAGE_FOOTER");
 
     StringBuilder sb = new StringBuilder();
     
     sb.Append(matrix_notifier_message_header);
-    
     sb.Append(Environment.NewLine);
-    sb.Append("- - -");
+    sb.Append(Environment.NewLine);
     sb.Append(fail2BanWebhook.Message);
     sb.Append(Environment.NewLine);
+    sb.Append("- - -");
+    sb.Append(Environment.NewLine);
+    sb.Append(matrix_message_footer);
 
     string message = sb.ToString();
 
